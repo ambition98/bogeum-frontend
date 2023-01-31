c5aed54b3cc9a30300d6d373b4ad5cfe056d9574
YJ_Lee <ambition65@naver.com>
2023-01-31 18:58:24 +0900

    회원가입 및 로그인 기능구현

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container style="max-width: 450px" fill-height justify-center>
+  <v-container class="login-form-container" fill-height justify-center>
     <v-flex xs8>
       <v-form
         ref="form"
@@ -10,7 +10,7 @@
           prepend-inner-icon="mdi-email"
           v-model="email"
           :rules="emailRules"
-          label="email"
+          label="이메일"
           class="input"
           required
         ></v-text-field>
@@ -20,7 +20,7 @@
           v-model="password"
           :rules="passwordRules"
           type="password"
-          label="password"
+          label="비밀번호"
           class="input"
           required
         ></v-text-field>
@@ -29,18 +29,37 @@
           color="success"
           @click="login"
           block
+          class="btn"
         >
           로그인
         </v-btn>
       </v-form>
+      <router-link to="/signup">
+        <v-btn
+          color="primary"
+          block
+          class="btn"
+        >
+          회원가입
+        </v-btn>
+      </router-link>
+      <a :href="googleRedirectUri">
+        <v-btn
+          color="primary"
+          block
+          class="btn"
+        >
+          구글
+        </v-btn>
+      </a>
     </v-flex>
   </v-container>
 </template>
 <script>
 export default {
-  components: {},
   data() {
     return {
+      googleRedirectUri: 'https://accounts.google.com/o/oauth2/v2/auth?client_id=257046050302-hhhha682d954b6a52v8ltmf6c3je4vv9.apps.googleusercontent.com&redirect_uri=' + window.location.protocol + '//' + window.location.host + '/afterlogin' + '&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email',
       valid: true,
       email: '',
       password: '',
@@ -49,23 +68,34 @@ export default {
         v => /.+@.+\..+/.test(v) || '이메일 형식이 잘못되었습니다'
       ],
       passwordRules: [
-        v => !!v || '필수항목입니다.'
-      ]
+        v => !!v || '필수항목입니다'
+      ],
+      googleCliendId: '257046050302-hhhha682d954b6a52v8ltmf6c3je4vv9.apps.googleusercontent.com'
     }
   },
-  setup() {},
-  created() {},
-  mounted() {},
-  unmounted() {},
   methods: {
-    login () {
+    async login() {
       const valid = this.$refs.form.validate()
       if (valid) {
-        // 로그인 시도
+        console.log(`email: ${this.email}`)
+        console.log(`password: ${this.password}`)
+        const resp = await this.$axios.post('/auth/login', {
+          email: this.email,
+          password: this.password
+        })
+        console.log(resp)
+        this.$store.dispatch('setUser', resp.data.dto)
+        // this.$router.go('/')
       }
     }
   }
 }
 </script>
 <style scoped>
+.login-form-container {
+  max-width: 450px
+}
+.btn {
+  margin-bottom: 5px;
+}
 </style>
